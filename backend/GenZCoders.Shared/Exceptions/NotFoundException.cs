3daf1093c6bcfb6e49cbaa8d767aabcb1e6ece97
Nour <nourelden.0523066@gmail.com>
2026-07-10 14:20:18 +0300

    feat: define custom global exceptions files

---
+++ backend/GenZCoders.Shared/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenZCoders.Shared.Exceptions
+{
+    public sealed class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
